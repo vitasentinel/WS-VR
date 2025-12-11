415f5589cfe98a680ff634c28fe12082576b268c
Ludloona59 <ludwig.obertetude@gmail.com>
2025-12-11 13:07:28 +0100

    animation de la ballerine (elle spine... pk?)

--- Projet/Assets/Mes trucs/Scènes/anmation/NewMonoBehaviourScript.cs
+++ Projet/Assets/Mes trucs/Scènes/anmation/NewMonoBehaviourScript.cs
@@ -1,32 +1,32 @@
 using Assets.Mes_trucs.Scènes.anmation;
 using Oculus.Interaction;
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.Windows;
 
-//public class NewMonoBehaviourScript : MonoBehaviour
-//{
-//    bool BonneOrdre = false;
-//    public class NewEmptyCSharpScript
-//    {
-
-
-//    }
-
-//    void Update()
-//    {  
-//        if(BonneOrdre = true)
-//        {
-//            //transform.position = Vector3.MoveTowards(
-//            //           transform.position*Ballerine,("sa descend");
-//            //           Position =-1);
-//            //(Ballerine.transform.position.y > transform.position.y) ;
-//            //Debug.Log("sa descend"); }
-//            if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
-//            {
-//                Debug.Log("La touche Espace a été pressée !");
-//            }
-//        }
-//    }
-//}
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public GameObject ballerine;
+    public float openRot, closeRot, speed;
+    public bool opening;
+    void Update()
+    {
+        Vector3 currentRot = ballerine.transform.localEulerAngles;
+        if (opening)
+        {
+            if (currentRot.y > openRot)
+            {
+                ballerine.transform.localEulerAngles = Vector3.Lerp(currentRot, new Vector3(currentRot.x, openRot, currentRot.z), speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (currentRot.y > closeRot)
+            {
+                ballerine.transform.localEulerAngles = Vector3.Lerp(currentRot, new Vector3(currentRot.x, openRot, currentRot.z), speed * Time.deltaTime);
+            }
+        }
+    }
+}
